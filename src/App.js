b01db1498c7c06d041bcebe8b2112059d65ee8a0
igoreshirokov <igoreshirokov@yandex.ru>
2021-10-11 08:40:56 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const srcBase = '/images/';
+  const fileType = '.jfif' || '.jpg';
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    setCards(() => {
+      let res = [];
+      function shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+      }
+      for (let i = 1; i <= 18; i++) {
+        res.push({ id: i, src: srcBase + i + fileType, view: false });
+        res.push({ id: i, src: srcBase + i + fileType, view: false });
+      }
+      shuffle(res);
+      return res;
+    });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory</h1>
+      <div onClick={() => console.log(cards)} className="game">
+        {cards.length > 0 ? cards.map((card) => {
+          return (
+            <div className={`visible`} id={`card-${card.id}`} key={`card-${card.src}`} className="card">
+              <img src={card.src} />
+            </div>
+          )
+        }) : <div className="loading">loading...</div>}
+      </div>
     </div>
   );
 }
