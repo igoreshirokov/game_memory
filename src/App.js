fa71f09d36380a65d03c3b2fc3455ae44fe25cde
igoreshirokov <igoreshirokov@yandex.ru>
2021-10-11 09:04:10 +0300

    hour

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const srcBase = '/images/';
   const fileType = '.jfif' || '.jpg';
   const [cards, setCards] = useState([]);
+  const [current, setCurrent] = useState(0);
+
   useEffect(() => {
     setCards(() => {
       let res = [];
@@ -14,20 +16,47 @@ function App() {
         array.sort(() => Math.random() - 0.5);
       }
       for (let i = 1; i <= 18; i++) {
-        res.push({ id: i, src: srcBase + i + fileType, view: false });
-        res.push({ id: i, src: srcBase + i + fileType, view: false });
+        res.push({dId: i, id: i, src: srcBase + i + fileType, view: false });
+        res.push({dId: i, id: 100+i, src: srcBase + i + fileType, view: false });
       }
       shuffle(res);
       return res;
     });
+    
   }, [])
+
+  function hiddenAll() {
+    setCards(cards.map( card => card.view = false ))
+  }
+
+  function hundlerClick(e) {
+    const target = e.target;
+    const id = target.id;
+
+    console.log(cards);
+    console.log(target);
+
+    setCurrent(id);
+    setCards(cards.map(card => {
+      if (card.id == id) {
+        card.view = true;
+      }
+    }))
+    setTimeout(() => {
+      console.log(current);
+      console.log('5 сек прошли');
+    }, 5000)
+  }
+
+
   return (
     <div className="App">
       <h1>Memory</h1>
-      <div onClick={() => console.log(cards)} className="game">
-        {cards.length > 0 ? cards.map((card) => {
+      <div onClick={hundlerClick} className="game">
+        {cards.length > 0 ? cards.map((card, i) => {
           return (
-            <div className={`visible`} id={`card-${card.id}`} key={`card-${card.src}`} className="card">
+            <div className={`card ${card.view ? 'visible' : 'hidden'}`} id={card.id} key={`card-${i + card.src}`}>
+              
               <img src={card.src} />
             </div>
           )
